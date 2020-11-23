9c6d1da63f848fade9f9c090a9ca24cd51c9f760
Najmul Huda <najmul.myself@gmail.com>
2020-11-23 13:12:17 +0600

    Ternary operator

---
+++ ternaryop.js
@@ -0,0 +1,14 @@
+var num1 = 10
+var num2 = 20
+
+/* if (num1 > num2){
+    console.log('num1 is greater than num2')
+}
+else{
+    console.log('num2 is greater than num1')
+} */
+
+//this simple thing in ternary operator
+
+var whoIsGreater = num1 > num2 ? 'Num1 is greater' : 'Num2 is greater'
+console.log(whoIsGreater)
