6952e16c1c4f11f0c9bb51861325f852e457cca6
Najmul Huda <najmul.myself@gmail.com>
2020-11-23 12:50:55 +0600

    This shows newDate

---
+++ switch.js
@@ -0,0 +1,29 @@
+var todayDate = new Date()
+console.log(todayDate)
+var newDate = todayDate.getDay()
+// console.log(newDate)
+
+switch(newDate){
+    case 0:
+        console.log('Sunday')
+        break
+    case 1:
+        console.log('Monday')
+        break
+    case 2:
+        console.log('Tuesday')
+        break
+    case 3:
+        console.log('Wednesday')
+        break
+    case 4:
+        console.log('Thrusday')
+        break
+    case 5:
+        console.log('Friday')
+        break
+    case 6:
+        console.log('Satarday')
+        break
+    
+}
