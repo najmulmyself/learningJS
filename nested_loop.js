66911479b6914ac645d1653bd38e7e0713999509
Najmul Huda <najmul.myself@gmail.com>
2020-11-27 19:37:21 +0600

    Pattern Wth nested loop

---
+++ nested_loop.js
@@ -0,0 +1,10 @@
+var n=10
+for(var i = 1;i<= n;i++){
+/*     console.log(i) */
+        var result = ''
+    for(var j = 1; j <=i; j++){
+        // console.log(j)
+        result += j + ''
+}
+        console.log(result)
+}
